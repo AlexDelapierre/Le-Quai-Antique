9f6050e08782f3e48172f026dfeb88f5a5ee8fb5
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-03-01 18:53:48 +0100

    Add SlugTrait and CategoriesFixtures

---
+++ src/DataFixtures/CategoriesFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class CategoriesFixtures extends Fixture
+{
+    private $counter = 1;
+    
+    public function __construct(private SluggerInterface $slugger){}
+
+    public function load(ObjectManager $manager): void
+    {
+        $parent = $this->createCategory('Entrée', null, $manager);
+        $parent = $this->createCategory('Plats', null, $manager);
+        $parent = $this->createCategory('Desserts', null, $manager);
+        
+       
+        $manager->flush();
+    }
+
+    public function createCategory(string $name, Category $parent = null, ObjectManager $manager)
+    {
+        $category = new Category();
+        $category->setName($name);
+        $category->setSlug($this->slugger->slug($category->getName())->lower());
+        $category->setParent($parent);
+        $manager->persist($category);
+
+        #Référence : mis en mémoire sous un nom d'un élément
+        $this->addReference('cat-'.$this->counter, $category);
+        $this->counter++; 
+        
+        return $category;
+    }
+}
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Trait\SlugTrait;
 use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,6 +11,8 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
 {
+    use SlugTrait;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -17,6 +20,13 @@ class Category
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'category')]
+    #[ORM\JoinColumn(onDelete: 'CASCADE')]
+    private ?self $parent = null;
+
+    #[ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class)]
+    private Collection $category;
 
     #[ORM\OneToMany(mappedBy: 'category', targetEntity: Plat::class, orphanRemoval: true)]
     private Collection $plats;
@@ -39,6 +49,48 @@ class Category
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getCategory(): Collection
+    {
+        return $this->category;
+    }
+
+    public function addCategory(self $category): self
+    {
+        if (!$this->category->contains($category)) {
+            $this->category->add($category);
+            $category->setParent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(self $category): self
+    {
+        if ($this->category->removeElement($category)) {
+            // set the owning side to null (unless already changed)
+            if ($category->getParent() === $this) {
+                $category->setParent(null);
+            }
+        }
 
         return $this;
     }
