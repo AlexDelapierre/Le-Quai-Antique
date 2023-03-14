6b2979c40c4c3499be8b8613fa10b6dc3de21f6d
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-03-14 16:13:40 +0100

    update AdminController

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin', name: 'app_admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/index.html.twig');
     }
 }
