8b81c191731bf8347b0cbf585e49e78c3fb2b25a
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-03-14 13:27:18 +0100

    Add ConfigSiteFixtures.php

--- src/DataFixtures/ConfigSiteFixtures.php
+++ src/DataFixtures/ConfigSiteFixtures.php
@@ -10,7 +10,46 @@ class ConfigSiteFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $horaires = '';
+        $horaires = 
+            '<h2>Horaires d\'ouverture</h2>
+            <table class="table">
+            <thead>
+                <tr>
+                <th></th>
+                <th></th>          
+                </tr>          
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Lundi</td>
+                    <td>12:00 - 14:00<br>19:00 - 22:00</td
+                </tr>
+                <tr>
+                    <td>Mardi</td>
+                    <td>12:00 - 14:00<br>19:00 - 22:00</td
+                </tr>
+                <tr>
+                    <td>Mercredi</td>
+                    <td>fermé</td
+                </tr>
+                <tr>
+                    <td>Jeudi</td>
+                    <td>12:00 - 14:00<br>19:00 - 22:00</td
+                </tr>
+                <tr>
+                    <td>Vendredi</td>
+                    <td>12:00 - 14:00<br>19:00 - 22:00</td
+                </tr>
+                <tr>
+                    <td>Samedi</td>
+                    <td>12:00 - 14:00<br>19:00 - 23:00</td
+                </tr>
+                <tr>
+                    <td>Dimanche</td>
+                    <td>fermé</td
+                </tr>         
+            </tbody>
+            </table>';
         $plat = new ConfigSite();
         $plat->setMaxCouverts(20);
         $plat->setHoraires($horaires);
