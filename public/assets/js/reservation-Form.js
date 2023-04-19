8878aa284b3f6ddde31b4db0b9946698f6cf0696
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-04-19 23:01:44 +0200

    Fix bug with nb couverts prefilled

--- public/assets/js/reservation-Form.js
+++ public/assets/js/reservation-Form.js
@@ -8,10 +8,12 @@ let midiDiv = document.getElementById("midiDiv");
 let soirDiv = document.getElementById("soirDiv");
 let nbCouvertsDiv = document.getElementById("nbCouvertsDiv");
 let fullAlert = document.getElementById("fullAlert");
+let nbCouvertsPrefilled = document.getElementById("reservation_nbCouverts").value;
 // let newReservationBottom = document.getElementById("newReservationBottom");
 
 soir.disabled = true;
 
+//Pour afficher ou cacher les créneaux midi ou soir en fonction de la séléction du service.
 service.addEventListener("change", (event) => {
   if (event.target.value === "midi") {
     midiDiv.style.display = "block";
@@ -33,11 +35,12 @@ let nbCouvertsRemain;
 let isWarningDisplayed = false;
 let warningParagraph;  
 
-// Définir la fonction preventDefaultSubmit
+// Définir la fonction preventDefaultSubmit pour empêcher la soumission du formulaire.
 function preventDefaultSubmit(event) {
   event.preventDefault();
 }
 
+// On va chercher le nb de couverts max et le nb de réservation total en base de donnée.
 fetch("/nbCouvertsMax")
   .then((response) => {
     if (!response.ok) {
@@ -60,11 +63,32 @@ fetch("/nbCouvertsMax")
     nbCouvertsRemain = nbCouvertsMax - nbCouvertsReservation;
     // console.log("Il reste:", nbCouvertsRemain, "places.");
 
+    //Si le nb de couverts restant n'est pas > 0, on affiche le message d'alerte et on cache le formulaire. 
     if (!nbCouvertsRemain > 0) {
       fullAlert.style.display = "block";
       formDiv.style.display = "none";
     }
 
+    //Si le nb de couverts restant - le nb de couvert pré-rempli sur le formulaire n'est pas > 0, 
+    //on affiche le message d'alerte et on empêche la soumission du formulaire.
+    if(!(nbCouvertsMax - nbCouvertsReservation - nbCouvertsPrefilled > 0)) {
+      // Ajouter l'événement "submit" avec la fonction de rappel nommée
+      form.addEventListener('submit', preventDefaultSubmit);
+      warningParagraph = document.createElement('p');
+      if (nbCouvertsRemain === 1){
+        warningParagraph.innerText = `Nous sommes désolé, il ne reste plus que ${nbCouvertsRemain} place !`;
+      } else if (nbCouvertsRemain !== 0) {
+        warningParagraph.innerText = `Nous sommes désolé, il ne reste plus que ${nbCouvertsRemain} places !`; 
+      } else {
+        warningParagraph.innerText = "Nous sommes désolé, il ne reste plus de places !";
+      }
+      warningParagraph.style.color = "red";
+      nbCouvertsDiv.append(warningParagraph);
+      isWarningDisplayed = true;
+    } 
+
+    //On écoute l'input nb de couverts et si la valeur rentré par le client et > au nb de couverts...
+    //restant, on affiche le message d'alerte et on empêche la soumission du formulaire. 
     nbCouverts.addEventListener("input", function(event) {  
       if (event.target.value > nbCouvertsRemain) {
 
@@ -73,9 +97,11 @@ fetch("/nbCouvertsMax")
         
         if(!isWarningDisplayed) {
           warningParagraph = document.createElement('p');
-          if(nbCouvertsRemain !== 0) {
-            warningParagraph.innerText = `Nous sommes désolé, il ne reste plus que ${nbCouvertsRemain} places !`;
-          }else {
+          if (nbCouvertsRemain === 1){
+            warningParagraph.innerText = `Nous sommes désolé, il ne reste plus que ${nbCouvertsRemain} place !`;
+          } else if (nbCouvertsRemain !== 0) {
+            warningParagraph.innerText = `Nous sommes désolé, il ne reste plus que ${nbCouvertsRemain} places !`; 
+          } else {
             warningParagraph.innerText = "Nous sommes désolé, il ne reste plus de places !";
           }
           warningParagraph.style.color = "red";
