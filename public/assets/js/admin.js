f7887f983d84090e0520b69cec7facf1af7a222e
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2023-07-26 17:45:43 +0200

    fix bug with table-responsive Bootstrap class in js

---
+++ public/assets/js/admin.js
@@ -0,0 +1,17 @@
+// Pour ajouter dynamiquement la classe Bootstrap tableResponsive aux tables du panneau d'administration en format mobile
+const tableResponsive = document.querySelector('.tableResponsive');
+
+function setClass() {
+  if (window.innerWidth > 992) {
+    
+    tableResponsive.classList.remove('table-responsive');
+  } else {
+    
+    tableResponsive.classList.add('table-responsive');
+  }
+}
+
+window.addEventListener('resize', setClass);
+setClass();
+
+
